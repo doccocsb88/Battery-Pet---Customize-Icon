049b3a0015c51aac171c14dc8178bef4fa9b8b16
haivu <haivu@vulcanlabs.co>
2026-04-29 14:09:46 +0700

    Add 2 wallpaper PAD packs
    - Rainy Solitude (7617564633288674573)
    
    - Chibi Ninja Legends (7632709597635071240)

---
+++ app_pack/wallpaper_pack_7617564633288674573/build.gradle.kts
@@ -0,0 +1,10 @@
+plugins {
+    id("com.android.asset-pack")
+}
+
+assetPack {
+    packName.set("wallpaper_7617564633288674573")
+    dynamicDelivery {
+        deliveryType.set("on-demand")
+    }
+}
---
+++ app_pack/wallpaper_pack_7632709597635071240/build.gradle.kts
@@ -0,0 +1,10 @@
+plugins {
+    id("com.android.asset-pack")
+}
+
+assetPack {
+    packName.set("wallpaper_7632709597635071240")
+    dynamicDelivery {
+        deliveryType.set("on-demand")
+    }
+}
